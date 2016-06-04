a830974f4905ddb39d83016f9591221741786de0
Thomas M. Hinkle <tmhinkle@gmail.com>
2016-06-04 08:19:30 -0400

    Update to point to version owned by @innovationcharter.org account for admin sdk to work

--- pull_from_google.sh
+++ pull_from_google.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-gdget.py -f json 1X49vghenZ-dZLJuQGxekP0DL2clU6VfLp41FgwZXaJvJr7fcU_cizgf4
+#gdget.py -f json 1X49vghenZ-dZLJuQGxekP0DL2clU6VfLp41FgwZXaJvJr7fcU_cizgf4
+gdget.py -f json 1kB-qfI7oDio3iWFszxwOlBG9xp4-IWisXCVgI6Ggm-i7-0dOC87amibB
