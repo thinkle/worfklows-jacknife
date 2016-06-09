3970b307c490c2ae8156b893f2e37f8ac358739d
Thomas M. Hinkle <tmhinkle@gmail.com>
2016-06-08 21:45:21 -0400

    Add basic calendar functionality.

--- Calendar.js
+++ Calendar.js
@@ -1,3 +1,20 @@
-function myFunction() {
-  
+function addUserToCalendar (user, calendarId) {
+  var calendarId = calendarId;
+  var acl = {
+    scope: {
+      type: 'user',
+      value: user
+    },
+    role: 'writer'
+  };
+  Calendar.Acl.insert(acl, calendarId);
 }
+
+function testAddUser () {
+	user = 'hs@innovationcharter.org'
+	allSchool =  'innovationcharter.org_4f5nt4qijeoblj11aj2q7hibdc@group.calendar.google.com' // All School
+	addUserToCalendar(user,allSchool);
+	hsCal = 'innovationcharter.org_0a0e0ddepor9shl5kfsvsvbt4c@group.calendar.google.com';
+	addUserToCalendar(user,hsCal);
+}
+
