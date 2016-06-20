65f35a5ce7dffa65933a6e20d325db426eb8dabf
Thomas M. Hinkle <tmhinkle@gmail.com>
2016-06-20 08:39:38 -0400

    Update log code to ignore duplicate field names

--- Logging.js
+++ Logging.js
@@ -1,12 +1,25 @@
 function handleMagicSettings (settings) {
+	// Where we allow magic syntax for magical things :)
+	return settings
 }
 
 
 function createLogHeaderRow (form) {
-  row = []
+  row = ['ResponseId','Timestamp']
+	ignoreTypes = [FormApp.ItemType.SECTION_HEADER,
+								 FormApp.ItemType.PAGE_BREAK]
   //form = FormApp.openById() // Comment me
-  form.getItems().forEach( function (item) {        
-    row.push(item.getTitle())
+  form.getItems().forEach( function (item) {
+		if (ignoreTypes.indexOf(item.getType()) == -1) {
+			if (row.indexOf(item.getTitle()) == -1) {
+				// Don't add duplicate header names.
+				// We assume that duplicate names are part of
+				// multi-page flows that are intended to provide
+				// one field value from two different points in a
+				// flow through a form as part of a UI
+				row.push(item.getTitle());
+			}
+		}
   }) // end forEach item
   return row
 }
@@ -24,7 +37,8 @@ function setupLoggingSheet (form, logSheet) {
   else {
     // If there is an existing sheet... grab the first row...
     logSheet = SpreadsheetApp.getActiveSheet() // comment me
-    var headers = logSheet.getRange(1,1,1,logSheet.getLastColumn())
+    var headers = logSheet.getRange(1,1,1,
+																		logSheet.getLastColumn())
     headers.forEach( function (i) {
       if (getItemByTitle(form,i)) {
         configOptions[i]='%'+i;
