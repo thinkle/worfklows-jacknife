99e90b6c1b15593df6a08615585f6e0cb8af33e5
Thomas M. Hinkle <tmhinkle@gmail.com>
2016-06-27 19:24:17 -0400

    working code for running after-the-fact to look for failed approval triggers

--- Approval.js
+++ Approval.js
@@ -103,19 +103,105 @@ function getApprovalFormToMasterLookup (actionRow) {
   return f2f
 }
 
+function getApprovalFormToMasterLookup (actionRow) {
+  // We expect the first sheet to have our lookup info...
+  var f2f = {}
+  var conf = actionRow['Config1'].table
+  if (conf) {
+    if (conf.toFields){
+			// Now we build out our array in a bit of a strange way...
+			for (var i=0; i<conf.toFields.length; i++) {
+				if (conf.fromFields.length > i) {
+					var fromField = conf.fromFields[i];
+					if (fromField) {
+						f2f[conf.toFields[i]] = fromField
+					}
+				}
+			} // end for loop...
+    }}
+  else {
+    Logger.log('No conf ?');
+    Logger.log('conf: '+JSON.stringify(conf));
+  }
+  return f2f
+}
+
 function checkForApprovals (form, logsheet) {
 	var allResponses = form.getResponses();
-	var logTable = Table(logsheet, 'OriginalResponseId');
+	var logTable = Table(logsheet.getDataRange(), 'OriginalResponseId');
 	var fixed = []
 	allResponses.forEach (function (resp) {
 		var respId = resp.getId();
-		if (! logTable.hasRow(respId)) {
+        // Here's some seriously f*ed up code thanks to google -- guess what, each form ID
+        // can actually be one of TWO different IDs, and the ID you get off of an object
+        // from resp.getId() is DIFFERENT than the ID you get from 
+        // form.getResponse(resp).getId() -- so yeah, thanks google, thanks a fucking lot.
+		if (! logTable.hasRow(respId) && ! logTable.hasRow(form.getResponse(resp.getId()))) {
 			// We don't have the row! We better do this thing...
 			Logger.log('Do this thing! '+respId+' '+resp);
+            //foo = y + 7 + 23;
 			//
+			fakeEvent = {
+				'source':form,
+				'response':resp,
+			}
+			logNormal('checkForApprovals triggering fakeEvent: %s',fakeEvent);
+			onFormSubmitTrigger(fakeEvent);
+		}
+		else {
+			logNormal('checkForApprovals found perfectly good row: %s',respId);
 		}
 	}); // end forEach resp
 }
 
 function approvalOnTimerCleanup () {
+	form_sources = []
+	ScriptApp.getProjectTriggers().forEach(function (trigger) {
+		if (trigger.getEventType()==ScriptApp.EventType.ON_FORM_SUBMIT) {
+			var formSource = trigger.getTriggerSourceId();
+			if (form_sources.indexOf(formSource)==-1) {
+				form_sources.push(formSource) // add to our list of form sources...
+			}
+		}
+	}) // end forEach trigger
+	var props = PropertiesService.getUserProperties()    
+	form_sources.forEach(function (fs) {
+		var masterID = props.getProperty(fs)
+		try {
+          logNormal('approvalOnTimerCleanup grabbing MASTER %s',masterID);
+			var masterSheet = SpreadsheetApp.openById(masterID)
+		}
+		catch (err) {
+          Logger.log('Error grabbing SS %s for %s',masterID,fs);
+          return
+		}
+		var masterConfig = getMasterConfig(masterSheet);
+		masterConfig.getConfigsForId(fs).forEach( function (row) {
+			if (row.Action=='Approval') {
+				Logger.log('We should do something with Approval row: %s',row)
+				logSS = SpreadsheetApp.openById(row['Config3'].table.SpreadsheetId);
+				logSheet = getSheetById(logSS, row['Config3'].table.SheetId);
+				checkForApprovals(FormApp.openById(fs),logSheet);
+			}
+		});
+	}) // end forEach formSource
+}
+			
+function listProps () {
+  var allProps = PropertiesService.getUserProperties().getProperties();  
+  for (var key in allProps) {Logger.log('%s:%s',key,allProps[key]);}
+  
+}
+function startoverProp () {
+    PropertiesService.getUserProperties().deleteProperty('FY16-06-###');
+    var scriptCache = CacheService.getScriptCache();
+    scriptCache.remove('FY16-06-###');
+    }
+    
+function testWeirdness () {
+   form = FormApp.openById('1HXV-wts968j0FqRFTkPYK8giyeSoYz_yjooIL9NqUVM');
+   existingResp = '2_ABaOnucOVov0flUTCRKnY938U4Qw3q3lEFKd3QYkY_gNFHOcSrn5TO1ZmUz9q0o'
+   Logger.log('existing id=>%s',form.getResponse(existingResp).getId());   
+   missingResp = 'ChMzMDQxODYzNjc5OTk2MjI3MzUzEOaJ4oW8yJGLjwE'
+   Logger.log('missing id=>%s',form.getResponse(missingResp).getId());
 }
