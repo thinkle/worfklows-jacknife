e03ad970d426c34cf7e514a4898e444c2f2fc1d6
Thomas M. Hinkle <tmhinkle@gmail.com>
2016-09-15 13:13:09 -0400

    Begin implementing add-on interface

--- Code.js
+++ Code.js
@@ -1,9 +1,32 @@
-function doGet() {
+/*function doGet() {
   var html = HtmlService.createTemplateFromFile('dataInterface');
   return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME);
+}*/
+
+function onOpen (e) {
+  SpreadsheetApp.getUi().createAddonMenu()  
+  .addItem('Open Workflows','showSidebar')
+  .addToUi();
+}
+
+function showSidebar () {
+  var ui = HtmlService.createHtmlOutputFromFile('Sidebar')
+  .setTitle('Jacknife');
+  SpreadsheetApp.getUi().showSidebar(ui);
+}
+
+
+function onInstall (e) {
+  onOpen(e)
 }
 
 function include(filename) { 
   return HtmlService.createHtmlOutputFromFile(filename)
       .getContent();
+}
+
+function createApprovalFormFromId (formId) {
+  Logger.log('createApprovalForm(%s)',formId);
+  firstForm = FormApp.openById(formId);
+  createApprovalForm (firstForm);
 }
