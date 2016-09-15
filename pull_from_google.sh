912ca4109e5af38d81ed5828281a7e0dac46b95e
Thomas M. Hinkle <tmhinkle@gmail.com>
2016-09-15 13:17:47 -0400

    new id

--- pull_from_google.sh
+++ pull_from_google.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #gdget.py -f json 1X49vghenZ-dZLJuQGxekP0DL2clU6VfLp41FgwZXaJvJr7fcU_cizgf4
-gdget.py -f json 1kB-qfI7oDio3iWFszxwOlBG9xp4-IWisXCVgI6Ggm-i7-0dOC87amibB
+#gdget.py -f json 1kB-qfI7oDio3iWFszxwOlBG9xp4-IWisXCVgI6Ggm-i7-0dOC87amibB
+gdget.py -f json 1rWoDJhnstrwiN3MBZXOJjg5yX2zsoNLlZLRO-Vp7gSX1za7UNSzENJBO
+
 date
