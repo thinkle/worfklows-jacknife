9f3b2d3b028e6380bf166812f3bd9fc86e4ebd7f
Thomas M. Hinkle <tmhinkle@gmail.com>
2016-09-26 11:50:31 -0400

    simplify

--- PickerHelper.js
+++ PickerHelper.js
@@ -1,14 +1,3 @@
-/**
- * Displays an HTML-service dialog in Google Sheets that contains client-side
- * JavaScript code for the Google Picker API.
- */
-function showPicker() {
-  var html = HtmlService.createHtmlOutputFromFile('Picker.html')
-      .setWidth(600)
-      .setHeight(425)
-      .setSandboxMode(HtmlService.SandboxMode.IFRAME);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Select a file');
-}
 
 /**
  * Gets the user's OAuth 2.0 access token so that it can be passed to Picker.
